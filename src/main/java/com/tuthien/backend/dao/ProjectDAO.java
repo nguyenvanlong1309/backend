ce7ad02e2104b73252ce7dacfcb1856b8db51c81
nguyenvanlong1309 <108638225+nguyenvanlong1309@users.noreply.github.com>
2023-01-05 22:49:17 +0700

    Upload

--- src/main/java/com/tuthien/backend/dao/ProjectDAO.java
+++ src/main/java/com/tuthien/backend/dao/ProjectDAO.java
@@ -18,19 +18,19 @@ public interface ProjectDAO extends JpaRepository<Project, String> {
 
     @Query("SELECT new com.tuthien.backend.model.ProjectModel(p)" +
             " FROM Project p" +
-            " WHERE p.cityId = ?1 AND p.status <> 0")
+            " WHERE p.cityId = ?1 AND p.status NOT IN (0,4,5)")
     List<ProjectModel> findByCityId(Integer cityId);
 
     @Query("SELECT new com.tuthien.backend.model.ProjectModel(p, u)" +
             " FROM Project p" +
             " JOIN User u ON u.username = p.createdBy" +
-            " WHERE p.status <> 0 AND p.status <> 5")
+            " WHERE p.status NOT IN (0,4,5)")
     List<ProjectModel> findApprovedProject(Pageable pageable);
 
     @Query("SELECT new com.tuthien.backend.model.ProjectModel(p)" +
             " FROM Project p" +
             " JOIN City c ON c.id = p.cityId" +
-            " WHERE c.regionId = ?1 AND YEAR(p.createdDate) = ?2")
+            " WHERE c.regionId = ?1 AND YEAR(p.createdDate) = ?2 and p.status <> 4" )
     List<ProjectModel> findByRegionIdAndYear(Long regionId, Integer year);
 
     @Query("SELECT new com.tuthien.backend.model.ProjectModel(p, u)" +
@@ -39,6 +39,9 @@ public interface ProjectDAO extends JpaRepository<Project, String> {
             " WHERE p.status = 0 AND (p.endDate > current_date OR p.type = 1) ")
     List<ProjectModel> findPendingProject();
 
-    @Query("SELECT COUNT(p) FROM Project p WHERE p.status <> 0")
+    @Query("SELECT COUNT(p) FROM Project p WHERE p.status <> 0 and p.status <> 4")
     Long countApprovedProject();
+
+    @Query("SELECT p FROM Project p WHERE p.status NOT IN (0,4) GROUP BY p.cityId")
+    List<Project> findByGroupByCityId();
 }
